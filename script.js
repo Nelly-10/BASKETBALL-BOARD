618fa38f5fb723150b9e5b2856bfbf5f84cf9a5b
NELLY-10 <nelson__nonso@yahoo.com>
2023-03-13 12:30:32 +0100

    new adopt

--- script.js
+++ script.js
@@ -1,90 +1,42 @@
 'use strict';
 
-// const btnHome1 = document.querySelector('.btn-1');
-// const btnHome2 = document.querySelector('.btn-2');
-// const btnHome3 = document.querySelector('.btn-3');
-// const homeScore = document.getElementById('home-score');
+const overAllHome = document.getElementById('home-score');
+const overAllGuess = document.getElementById('guest-score');
 
 
-// let btns = 0;
+let homeScore = 0;
+let guessScore = 0;
 
-
-// btnHold1.addEventListener('click', function() {
-//     btns += 1;
-//     document.getElementById('home-score').textContent = btns;
-// });
-
-// const homeScore = document.getElementById("home-score");
-// const guestScore = document.getElementById("guest-score");
-
-// let initialHomeScore = 0;
-// let initialGuestScore = 0;
-
-// function haddone() {
-//     initialHomeScore += 1;
-//     homeScore.textContent = initialHomeScore;
-// }
-
-// function gaddone() {
-//     initialGuestScore +=1
-//     guestScore.textContent = initialGuestScore;
-// }
-
-// function haddtwo() {
-//     initialHomeScore += 2
-//     homeScore.textContent = initialHomeScore;
-// }
-
-// function gaddtwo() {
-//     initialGuestScore +=2
-//     guestScore.textContent = initialGuestScore;
-// }
-
-// function haddthree() {
-//     initialHomeScore += 3
-//     homeScore.textContent = initialHomeScore;
-// }
-
-// function gaddthree() {
-//     initialGuestScore +=3
-//     guestScore.textContent = initialGuestScore;
-// }
-
-
-
-const homeScore = document.getElementById('home-score');
-const guestScore = document.getElementById('guest-score');
-
-
-let hScore = 0;
-let gScore = 0;
-
-function gaddOne() {
-    hScore += 1;
-    document.getElementById('home-score').textContent = hScore;
+function hOne() {
+    homeScore = homeScore + 1;
+    overAllHome.textContent = homeScore;
 }
 
-function gaddTwo() {
-    hScore += 2;
-    document.getElementById('home-score').textContent = hScore;
+function hTwo() {
+    homeScore = homeScore + 2;
+    overAllHome.textContent = homeScore;
 }
 
-function gaddThree() {
-    hScore += 3;
-    document.getElementById('home-score').textContent = hScore;
+function hThree() {
+    homeScore = homeScore + 3;
+    overAllHome.textContent = homeScore;
 }
 
-function haddOne() {
-    gScore += 1;
-    document.getElementById('guest-score').textContent = gScore;
+
+
+function gOne() {
+    guessScore = guessScore + 1;
+    overAllGuess.textContent = guessScore;
 }
 
-function haddTwo() {
-    gScore += 2;
-    document.getElementById('guest-score').textContent = gScore;
+function gTwo() {
+    guessScore = guessScore + 2;
+    overAllGuess.textContent = guessScore;
 }
 
-function haddThree() {
-    gScore += 3;
-    document.getElementById('guest-score').textContent = gScore;
+function gThree() {
+    guessScore = guessScore + 3;
+    overAllGuess.textContent = guessScore;
 }
+
+
